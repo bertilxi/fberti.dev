a9dbe42a1b98f6b4371e529ff402bdd26c0369cd
Fernando Berti <bertilxi@gmail.com>
2019-03-31 22:04:43 -0300

    replaced google font with typeface

--- pages/index.vue
+++ pages/index.vue
@@ -5,5 +5,8 @@
         Hello, I am Fernando, and this is my website.
       </strong>
     </h3>
+    <p>
+      I am a frontend developer, an UI engineer, a webpack tuner.
+    </p>
   </div>
 </template>
